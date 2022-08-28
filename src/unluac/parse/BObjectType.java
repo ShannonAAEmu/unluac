9887ed995d13f507cbe4bb07aabd195bc39a23ce
Shannon Max <jeyms_bont@mail.ru>
2022-08-28 21:17:29 +0300

    Add support x64 compilation

--- src/unluac/parse/BObjectType.java
+++ src/unluac/parse/BObjectType.java
@@ -16,14 +16,7 @@ abstract public class BObjectType<T extends BObject>  {
   public final BList<T> parseList(final ByteBuffer buffer, final BHeader header) {
     BInteger length = header.integer.parse(buffer, header);
     final List<T> values = new ArrayList<T>();
-    length.iterate(new Runnable() {
-      
-      @Override
-      public void run() {
-        values.add(parse(buffer, header));
-      }
-      
-    });
+    length.iterate(() -> values.add(parse(buffer, header)));
     return new BList<T>(length, values);
   }
   
